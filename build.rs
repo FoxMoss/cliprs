e342d62d20c1e41c41f9bed4af1f893eca226ba0
FoxMoss <foxmoss@mediaology.com>
2026-01-07 12:58:46 -0600

    improve speeds

--- build.rs
+++ build.rs
@@ -6,18 +6,28 @@ fn main() {
         .flag("-Wno-sign-compare")
         .flag("-Wno-unused-variable")
         .flag("-mavx")
-        .flag("-mavx2")
+        // .flag("-mavx2")
+        // .flag("-mavx512f")_mm256_fmadd_ps
+        // .flag("-mavx512vbmi")
+        // .flag("-mavx512vnni")
         .flag("-mfma")
+        .flag("-mf16c")
         .flag("-O3")
         .compile("clipcpp");
-
+    
     cc::Build::new()
         .file("ggml/src/ggml.c")
         .flag("-std=c11")
         .flag("-Wno-unused-variable")
-        // .flag("-DGGML_USE_ACCELERATE")
-        // .flag("-DGGML_USE_CLBLAST")
+        .flag("-mavx")
+        // .flag("-mavx2")
+        // .flag("-mavx512f")
+        // .flag("-mavx512vbmi")
+        // .flag("-mavx512vnni")
+        .flag("-mfma")
+        .flag("-mf16c")
         .flag("-O3")
+        .flag("-DGGML_USE_OPENBLAS")
         .define("_GNU_SOURCE", None) 
         .define("_POSIX_C_SOURCE", "200809L")  
         .compile("ggml");
@@ -26,4 +36,6 @@ fn main() {
     println!("cargo:rerun-if-changed=rust_interface.cpp");
     println!("cargo:rerun-if-changed=clip.h");
     println!("cargo:rerun-if-changed=ggml/src/ggml.c");
+
+    println!("cargo:rustc-link-lib=cblas");
 }
